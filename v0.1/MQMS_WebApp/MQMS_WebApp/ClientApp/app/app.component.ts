ebf59cb38055f5aec2ad63ed0a43a170bdae99b6
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-10-31 11:34:37 +1300

    save current project

---
+++ v0.1/MQMS_WebApp/MQMS_WebApp/ClientApp/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from "@angular/core";
+@Component({
+    selector: "app-root",
+    template: `<h1>MPI Marine Site Quarantine Management System</h1><div>Work in progress...</div>`
+})
+export class AppComponent { }
--- v0.1/MQMS_WebApp/MQMS_WebApp/Startup.cs
+++ v0.1/MQMS_WebApp/MQMS_WebApp/Startup.cs
@@ -33,8 +33,9 @@ namespace MQMS_WebApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseMvc();
         }
+
     }
 }
