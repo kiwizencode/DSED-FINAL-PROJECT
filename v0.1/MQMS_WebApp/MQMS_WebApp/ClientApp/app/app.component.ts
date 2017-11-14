a2c8949431b60e77528fe26c0cc87dbf552fd484
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-14 20:10:27 +1300

    remove all old project and start from all over again

--- v0.1/MQMS_WebApp/MQMS_WebApp/ClientApp/app/app.component.ts
+++
@@ -1,6 +0,0 @@
-﻿import { Component } from "@angular/core";
-@Component({
-    selector: "app-root",
-    template: `<h1>MPI Marine Site Quarantine Management System</h1><div>Work in progress...</div>`
-})
-export class AppComponent { }
